55a6ebd11db51b688390161969286c34f048c44f
Pablo Rodriguez <pablorodriguez88@gmail.com>
2024-02-20 10:01:08 -0300

    remove gemfile ci_uy, and create new validation method

--- Gemfile
+++ Gemfile
@@ -29,9 +29,6 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
-# To validate CI
-gem "ci_uy"
-
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,6 @@
 #  updated_at    :datetime         not null
 #
 
-require 'ci_uy'
-
 class User < ApplicationRecord
   has_and_belongs_to_many :movies, class_name: 'Movie'
 
@@ -23,6 +21,9 @@ class User < ApplicationRecord
   validate :check_ci_format
   validate :check_age
 
+  before_validation :normalize_document
+  before_save :normalize_document_for_save
+
   enum document_type: [:ci, :dni, :passport, :other]
 
   def as_json(options = {})
@@ -30,10 +31,8 @@ class User < ApplicationRecord
   end
 
   def check_ci_format
-    if document_type == "ci"
-     unless CiUY.validate(document)
-       errors.add(:document, "CI must be a valid Uruguayan document ID number")
-     end
+    if document_type.to_sym == :ci
+      errors.add(:document, "Invalid format for CI") unless validateCiUY(document)
     end
   end
 
@@ -41,5 +40,30 @@ class User < ApplicationRecord
     if birthday.present? && birthday > 18.years.ago
       errors.add(:birthday, "You must be over 18 years old")
     end
+  end
+
+  def normalize_document
+    self.document = document.gsub(/[.-]/, '') if document.present?
+  end
+
+  def normalize_document_for_save
+    normalize_document
+  end
+
+  def validateCiUY(ci)
+    ci_array = ci.chars.map(&:to_i)
+ puts ci
+    return false unless ci_array.length == 8
+    sum = 0
+    multipliers = [2, 9, 8, 7, 6, 3, 4]
+
+    for i in 0..6
+      sum += ci_array[i] * multipliers[i]
+    end
+
+    mod = sum % 10
+    h = (10 - mod) % 10
+
+    return h == ci_array.last
   end
 end
